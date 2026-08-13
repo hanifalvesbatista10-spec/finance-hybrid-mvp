257f121daa30309df66d825a522bbd215d5b9628
hanifalvesbatista10-spec <hanif.alves.batista10@gmail.com>
2026-08-13 12:46:46 -0300

    Add financial accounts page

---
+++ app/dashboard/contas-financeiras/page.tsx
@@ -0,0 +1,5 @@
+import { FinancialAccountsManager } from "@/components/finance/FinancialAccountsManager";
+
+export default function FinancialAccountsPage(){
+  return <FinancialAccountsManager/>;
+}
